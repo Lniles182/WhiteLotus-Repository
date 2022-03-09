4bb38449c01ccd77234ef718e99d386d0a15841a
Lniles182 <90267283+Lniles182@users.noreply.github.com>
2022-03-09 10:12:23 -0600

    PoopenFarten

--- Death Crusade/Assets/Scripts/PlayerMovement.cs
+++ Death Crusade/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+	private RaycastHit2D hit;
+
+
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Creature", "Blocking"));
+        if (hit.collider == null)
+		{
+			transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+		}
     }
 }
